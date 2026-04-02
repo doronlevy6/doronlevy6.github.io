a5d3e48184ed8571c20d86c988b72f00c1669f85
דורון לוי <doronlevy66@gmail.com>
2026-04-03 01:12:11 +0300

    Deploy web build: 2026-04-03 01:12:11 IDT

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "0bd0b799d64c52ef71e284e1314f73be",
+const RESOURCES = {"flutter_bootstrap.js": "f0e1c93ef654f415cfc8fcd46cda175d",
 "version.json": "fcb3cc8362ebd69d041cddc007453272",
 "index.html": "e180d4ed67013ad967d5108955101c43",
 "/": "e180d4ed67013ad967d5108955101c43",
-"main.dart.js": "a8feabce016a87d554ac1e621e997198",
+"main.dart.js": "870b95eb9110e1b01e38e6b7da409674",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "010f86476a48717e4fd2cda68d4fb89e",
 "icons/Icon-192.png": "437617db87bc19d217a3f241c3096f5e",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "0bd0b799d64c52ef71e284e1314f73be",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "fb87a83023208cc02cf61bbbbf14b580",
-"assets/fonts/MaterialIcons-Regular.otf": "820853b8a05d23898d8334984015866b",
+"assets/fonts/MaterialIcons-Regular.otf": "2e22d8d885aaaaeb2f68bd7026b97a5a",
 "assets/assets/images/il-flag.png": "b05f2ec2103ccad1a8c2f83c2a41ac71",
 "assets/assets/images/ball-logo.png": "7347aafca43d34ddbe99415f6ab945f5",
 "assets/assets/images/app_icon.png": "fe2e61ab28ce72bf1519ac6da322d6f3",
