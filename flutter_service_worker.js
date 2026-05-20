4e8fee96813b6acc4c3f6a8c9bb6d3c10a4ff07a
דורון לוי <doronlevy66@gmail.com>
2026-05-20 22:50:23 +0300

    Deploy web build: 2026-05-20 22:50:23 IDT

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "1e11391ddc43511bf390b83768a80ceb",
+const RESOURCES = {"flutter_bootstrap.js": "ae012ed6a5e5d369eaa0a858848de9a1",
 "version.json": "fcb3cc8362ebd69d041cddc007453272",
 "index.html": "e180d4ed67013ad967d5108955101c43",
 "/": "e180d4ed67013ad967d5108955101c43",
-"main.dart.js": "627f3e7074353aa1bea0f45127a1b8fc",
+"main.dart.js": "dfec0c445d0ebeff93d36d885576f274",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "010f86476a48717e4fd2cda68d4fb89e",
 "icons/Icon-192.png": "437617db87bc19d217a3f241c3096f5e",
